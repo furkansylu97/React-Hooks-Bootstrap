d5b9a7c65d1e9a9a53cf20e1412518265ee88457
Furkan Soylu <furkansylu97@gmail.com>
2025-06-13 17:20:51 +0300

    The process of getting the written values was completed

--- TodoProject/src/components/CreateTask.jsx
+++ TodoProject/src/components/CreateTask.jsx
@@ -1,6 +1,23 @@
 import React from 'react'
+import { useState } from 'react'
 
 export const CreateTask = () => {
+
+  const [task, setTask] = useState({
+    title:'',
+    content:'',
+  })
+
+  function detectChange(event) {
+    const {name, value} = event.target;
+    setTask((prevTask) => {
+      return {
+        ...prevTask,
+        [name]:value,
+      }
+    })
+  }
+
   return (
     <div className='CreateTaskWrapper'>
         <form className='CreateTaskForm'>
@@ -9,12 +26,16 @@ export const CreateTask = () => {
             className='form-control TodoText mb-3'
             name='title'
             placeholder='Title'
+            value={task.title}
+            onChange={detectChange}
             />
 
             <textarea name="content"
             className='form-control TodoText mb-3'
             rows={3}
             placeholder='Please write your job'
+            value={task.content}
+            onChange={detectChange}
             ></textarea>
 
             <button className="btn btn-primary TodoButton">Add</button>
